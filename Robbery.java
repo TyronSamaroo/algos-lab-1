10fd0554cb0b1dda2edc4fe541fa5f303fefc09b
Tyron Samaroo <tyronsamaroo@gmail.com>
2019-03-21 17:07:35 -0400

    Added new parameter to fuction call to keep account of the item. This will help decrement the sizes of the worth of the object and make the resursive solution simple.

--- Robbery.java
+++ Robbery.java
@@ -18,46 +18,33 @@ int n;
 		int[] worths,
 		int n
 	) {
-
-
 		// fill in here, change the return
 		/*
 		* If my bag cant hold anything I have no value
 		* If theres no items no matter size of my bag I will have no value
 		* */
 		//Base Case
-
-
-		if(capacity == 0 || n == 0)
-		{
-			return 0;
-		}
-
-
-
+			if(capacity == 0 || n == 0)
+			{
+				return 0;
+			}
 		// If an item cannot fit in my bag. Go to the next item that can fit
-		if(sizes[n- 1] > capacity)
-
-			return maximizeRobWorthRecur(capacity,sizes,worths, n-1);
+			if(sizes[n- 1] > capacity)
+				return maximizeRobWorthRecur(capacity,sizes,worths, n-1);
 		// If all the sizes less than capacity
 		// If item can fit means below capacity that item will be added.
 		// The remaining capacity will be updated and the next size will be checked.
 		// It will find the max of the n-1 item with another item
-		else return max(worths[n-1] + maximizeRobWorthRecur(capacity-sizes[n-1],sizes,worths, n-1),
-				maximizeRobWorthRecur(capacity,sizes,worths, n-1)
-		);
+			else return max(worths[n-1] + maximizeRobWorthRecur(capacity-sizes[n-1],sizes,worths, n-1),
+				maximizeRobWorthRecur(capacity,sizes,worths, n-1));
+		}
 
-
-
-	}
-
-
-	public int max(int a, int b)
+	static int max(int a, int b)
 	{
 		return (a > b)? a : b;
 	}
 
-	public int maximizeRobWorthBottomUp(
+	static int maximizeRobWorthBottomUp(
 		int capacity,
 		int[] sizes,
 		int[] worths,
@@ -66,22 +53,18 @@ int n;
 		int i,j;
 		//Give correct size of table
 		int[][] DPTable = new int[n+1][capacity+1];
-		for ( i = 0; i < n; i++) {
-			for (j = 0; j < capacity ; j++) {
-
+		for ( i = 0; i <= n; i++)
+		{
+			for (j = 0; j <=capacity ; j++)
+			{
 				if( i == 0 || j == 0)
 					DPTable[i][j] = 0;
 				else if (sizes[i-1] <= j)
-					DPTable[i][j] = max(worths[i-1] + DPTable[i-1][j-sizes[i-1]],DPTable[i-1][j]);
+					DPTable[i][j] = max(worths[i-1] + DPTable[i-1][j-sizes[i-1]], DPTable[i-1][j]);
 				else
 					DPTable[i][j] = DPTable[i-1][j];
-
-
 			}
-			
 		}
-			
-
 		// fill in here, change the return
 		return DPTable[n][capacity];
 	}
